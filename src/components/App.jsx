89172d2f109281a0b7bb7a74c14aa6380b6f498f
Serhii <sergbr78@gmail.com>
2023-10-21 16:13:25 +0200

    dz7
    hauashabe7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,22 @@
-// import React from 'react';
-// import { nanoid } from 'nanoid';
 import { Form } from './Form';
-import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
-// import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { getContacts, removeContact } from './redux/contactsSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from './redux/operations';
+import { getContacts } from './redux/contactsSlice';
+import { useEffect, lazy } from 'react';
+
 // import { setStatusFilter } from './redux/filtersSlice';
+const { ContactList } = lazy(() => import('./ContactList/ContactList'));
 
 export function App() {
-  // console.log(contacts);
-
-  // const changeFilter = filter => {
-  //   () => dispatch(setStatusFilter(filter));
-  // };
-
-  // const removeContact = contactId => {
-  //   setContacts(contacts.filter(({ id }) => id !== contactId));
-  // };
-
-  // useEffect(() => {
-  //   const contactsList = localStorage.getItem('contacts');
-  //   const parsedContacts = JSON.parse(contactsList);
-
-  //   if (parsedContacts) {
-  //     store.dispatch(setContacts(parsedContacts));
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
+  const dispatch = useDispatch();
+  const { items, isLoading, error } = useSelector(getContacts);
+  console.log(items);
+  // Вызываем операцию
+  useEffect(() => {
+    dispatch(fetchContacts());
+    console.log(dispatch);
+  }, [dispatch]);
 
   return (
     <div
@@ -45,23 +31,15 @@ export function App() {
     >
       <h1>Phonebook</h1>
       <>
-        <Form
-        // onSubmit={formSubmitHandler}
-        />
+        <Form />
         <h2>Contacts</h2>
+        {<Filter />}
         {
-          <Filter
-          // value={filters}
-          // onChangeFilter={e => dispatch(setStatusFilter(filters))}
-          />
-        }
-        {
-          // visibleContacts.length > 0 && (
-          <ContactList
-          // contacts={visibleContacts}
-          // onRemoveContact={() => dispatch(removeContact())}
-          />
-          // )
+          <div>
+            {isLoading && <p>Loading contacts...</p>}
+            {error && <p>{error}</p>}
+            {items.length > 0 && <ContactList />}
+          </div>
         }
       </>
     </div>
